8c7d11ee27f7f8ebe8cbd6687ca6c577c312221e
hyebin-dev <229252157+hyebin-dev@users.noreply.github.com>
2026-01-07 11:45:41 +0900

    BOARD-4 회원가입 예외 처리 보강

---
+++ src/main/java/com/example/demo/repository/UserDAO.java
@@ -0,0 +1,29 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.AuthVO;
+import com.example.demo.domain.UserVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserDAO {
+
+    void insert(UserVO userVO);
+
+    void authInsert(String email);
+
+    UserVO getUser(String username);
+
+    List<AuthVO> getUserAuth(String username);
+
+    void modifyNoPwd(UserVO userVO);
+
+    void modify(UserVO userVO);
+
+    void authDelete(String name);
+
+    void remove(String name);
+
+    List<UserVO> getList();
+}
---
+++ src/main/java/com/example/demo/service/BoardServiceImpl.java
@@ -0,0 +1,53 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.BoardVO;
+import com.example.demo.domain.PagingVO;
+import com.example.demo.repository.BoardDAO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class BoardServiceImpl implements BoardService {
+    private final BoardDAO boardDAO;
+
+    @Override
+    public void insert(BoardVO boardVO) {
+        log.info(">>> insert >> {}", boardVO);
+        boardDAO.insert(boardVO);
+    }
+
+    @Override
+    public List<BoardVO> getList(PagingVO pagingVO){
+        return boardDAO.getList(pagingVO);
+    }
+
+    @Transactional
+    @Override
+    public BoardVO getDetail(long bno) {
+        boardDAO.readCountUpdate(bno, 1);
+        return boardDAO.getDetail(bno);
+    }
+
+    @Override
+    public int getTotalCount(PagingVO pagingVO) {
+        return boardDAO.getTotalCount(pagingVO);
+    }
+
+    @Transactional
+    @Override
+    public void update(BoardVO boardVO) {
+        boardDAO.readCountUpdate(boardVO.getBno(),-1);
+        boardDAO.update(boardVO);
+    }
+
+    @Override
+    public void delete(long bno) {
+        boardDAO.delete(bno);
+    }
+}
